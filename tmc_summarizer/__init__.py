d39ed189299fd91a67e778a423ec3e45a3aed99e
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-07-18 16:32:22 -0700

    oops.

--- tmc_summarizer/__init__.py
+++ tmc_summarizer/__init__.py
@@ -1,5 +1,5 @@
 from .data_model import TMC_File#, geocode_tmc
-#from .summarize import write_summary_file
+from .summarize import write_summary_file
 from .web_model import TMC_Webapp_Upload_File
 from .business_logic import *
 
