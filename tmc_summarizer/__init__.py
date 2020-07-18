4b42356faa89a23ad896ad35423c90de45ad28c6
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-07-18 15:41:55 -0700

    adding separate model specifically for uploading

--- tmc_summarizer/__init__.py
+++ tmc_summarizer/__init__.py
@@ -1,4 +1,6 @@
-from .data_model import TMC_File, geocode_tmc
-from .summarize import write_summary_file
+from .data_model import TMC_File#, geocode_tmc
+#from .summarize import write_summary_file
+from .web_model import TMC_Webapp_Upload_File
+from .business_logic import *
 
-__VERSION__ = "0.2.0"
+__VERSION__ = "0.2.1"
