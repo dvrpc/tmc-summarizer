1ab53ec2dd309b5cfb1a72f1daf9fe56e2b347f7
mmorley <mmorley0395@gmail.com>
2022-06-06 18:52:49 -0400

    need a peak hour factor revert point..

--- tmc_summarizer/data_model.py
+++ tmc_summarizer/data_model.py
@@ -153,10 +153,79 @@ class TMC_File:
                 self.end_time = row.time
 
         # Set the order of the columns (eb, wb, nb, sb)
-        self.total_cols = ['EB U', 'EB Left', 'EB Thru', 'EB Right', 'EB Xwalk Xings','WB U', 'WB Left', 'WB Thru', 'WB Right', 'WB Xwalk Xings', 'NB U', 'NB Left', 'NB Thru', 'NB Right', 'NB Xwalk Xings', 'SB U', 'SB Left', 'SB Thru', 'SB Right', 'SB Xwalk Xings', 'total_15_min', 'total_hourly']
-        self.car_cols = ['EB U', 'EB Left', 'EB Thru', 'EB Right', 'EB Peds Xwalk','WB U', 'WB Left', 'WB Thru', 'WB Right', 'WB Peds Xwalk', 'NB U', 'NB Left', 'NB Thru', 'NB Right', 'NB Peds Xwalk', 'SB U', 'SB Left', 'SB Thru', 'SB Right', 'SB Peds Xwalk', 'total_15_min', 'total_hourly']
-        self.heavy_cols = ['EB U', 'EB Left', 'EB Thru', 'EB Right', 'EB Bikes Xwalk','WB U', 'WB Left', 'WB Thru', 'WB Right', 'WB Bikes Xwalk', 'NB U', 'NB Left', 'NB Thru', 'NB Right', 'NB Bikes Xwalk', 'SB U', 'SB Left', 'SB Thru', 'SB Right', 'SB Bikes Xwalk', 'total_15_min', 'total_hourly']
-        
+        self.total_cols = [
+            "EB U",
+            "EB Left",
+            "EB Thru",
+            "EB Right",
+            "EB Xwalk Xings",
+            "WB U",
+            "WB Left",
+            "WB Thru",
+            "WB Right",
+            "WB Xwalk Xings",
+            "NB U",
+            "NB Left",
+            "NB Thru",
+            "NB Right",
+            "NB Xwalk Xings",
+            "SB U",
+            "SB Left",
+            "SB Thru",
+            "SB Right",
+            "SB Xwalk Xings",
+            "total_15_min",
+            "total_hourly",
+        ]
+        self.car_cols = [
+            "EB U",
+            "EB Left",
+            "EB Thru",
+            "EB Right",
+            "EB Peds Xwalk",
+            "WB U",
+            "WB Left",
+            "WB Thru",
+            "WB Right",
+            "WB Peds Xwalk",
+            "NB U",
+            "NB Left",
+            "NB Thru",
+            "NB Right",
+            "NB Peds Xwalk",
+            "SB U",
+            "SB Left",
+            "SB Thru",
+            "SB Right",
+            "SB Peds Xwalk",
+            "total_15_min",
+            "total_hourly",
+        ]
+        self.heavy_cols = [
+            "EB U",
+            "EB Left",
+            "EB Thru",
+            "EB Right",
+            "EB Bikes Xwalk",
+            "WB U",
+            "WB Left",
+            "WB Thru",
+            "WB Right",
+            "WB Bikes Xwalk",
+            "NB U",
+            "NB Left",
+            "NB Thru",
+            "NB Right",
+            "NB Bikes Xwalk",
+            "SB U",
+            "SB Left",
+            "SB Thru",
+            "SB Right",
+            "SB Bikes Xwalk",
+            "total_15_min",
+            "total_hourly",
+        ]
+
         # Read the DATA tabs into dataframes
         # ----------------------------------
         self.df_cars = self.read_data_tab("Cars")
@@ -215,8 +284,8 @@ class TMC_File:
             skiprows=3,
             header=None,
             names=self.flatten_headers(tabname),
-            sheet_name=tabname
-        ).dropna(subset=['SB U'])
+            sheet_name=tabname,
+        ).dropna(subset=["SB U"])
 
         df["datetime"] = None
 
@@ -225,7 +294,7 @@ class TMC_File:
             date_holder = datetime.date(date_holder)
             try:
                 row.time = datetime.time(row.time)
-            except: 
+            except:
                 row.time = row.time
             df.at[idx, "datetime"] = datetime.combine(self.date, row.time)
 
@@ -345,7 +414,7 @@ class TMC_File:
 
         return df
 
-    def get_peak_hour(self, period: str) -> (datetime, datetime):
+    def get_peak_hour(self, period: str):
         """
         Identify start / end times of the AM or PM peak hour.
         Return this information as a tuple with each value as a datetime.
@@ -374,6 +443,26 @@ class TMC_File:
         start = end - timedelta(hours=1)
 
         return start, end
+
+    def peak_hour_factor(self, period: str):
+
+        noon = datetime.combine(self.date, time(hour=12))
+        period = period.upper()
+
+        if period == "AM":
+            df = self.df_total[self.df_total.index < noon]
+        elif period == "PM":
+            df = self.df_total[self.df_total.index >= noon]
+        else:
+            print("Period must be AM or PM")
+            return
+
+        # idx = df[["total_hourly"]].idxmax()[0]
+        # print(idx)
+        # print(df.index.name)
+
+        # df[df.index < idx].tail(3)
+        return df.head(60)
 
     def peak_hour_text(self, period: str) -> str:
         """
@@ -467,9 +556,9 @@ class TMC_File:
 
         return dff
 
-    def treemap_df(self,
-                   start_time: str = "7:00",
-                   end_time: str = "12:00") -> pd.DataFrame:
+    def treemap_df(
+        self, start_time: str = "7:00", end_time: str = "12:00"
+    ) -> pd.DataFrame:
         """
         Create a dataframe that can be dropped into a plotly treemap figure.
 
@@ -487,7 +576,9 @@ class TMC_File:
         # Get the full raw dataset
         df = self.all_raw_data()
 
-        dff = self.filter_df_by_start_end_time(df, start_time=start_time, end_time=end_time)
+        dff = self.filter_df_by_start_end_time(
+            df, start_time=start_time, end_time=end_time
+        )
 
         # Sum all values so there's one row instead of one for every 15 minutes
         treemap_df = pd.DataFrame(dff.sum())
@@ -508,9 +599,9 @@ class TMC_File:
             leg = split_value[1]
             movement = " ".join(split_value[2:])
 
-            if 'Peds' in movement or 'Bikes' in movement:
-                wt = movement.split(' ')[0]
-                movement = 'Xwalk'
+            if "Peds" in movement or "Bikes" in movement:
+                wt = movement.split(" ")[0]
+                movement = "Xwalk"
 
             treemap_df.at[idx, "wt"] = wt
             treemap_df.at[idx, "leg"] = leg
