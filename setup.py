ca784ecf4205be0b7b0fe437782c4142d0197360
mmorley0395 <77309089+mmorley0395@users.noreply.github.com>
2022-11-03 13:19:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='tmc_summarizer',
     packages=find_packages(),
-    version='0.1.1',
+    version='1.0.0',
     description='Process raw TMC data into more functional formats',
-    author='Aaron Fraint, AICP',
+    author='Aaron Fraint, AICP; Mark Morley, AICP', 
     entry_points="""
         [console_scripts]
         tmc=tmc_summarizer.cli:main
