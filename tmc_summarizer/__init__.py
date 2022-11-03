1388823c4786496a53d898f8c87385729ba8e766
mmorley0395 <77309089+mmorley0395@users.noreply.github.com>
2022-11-03 13:16:38 -0400

    Update __init__.py

--- tmc_summarizer/__init__.py
+++ tmc_summarizer/__init__.py
@@ -1,6 +1,6 @@
-from .data_model import TMC_File#, geocode_tmc
+from .data_model import TMC_File
 from .summarize import write_summary_file
 from .web_model import TMC_Webapp_Upload_File
 from .business_logic import *
 
-__VERSION__ = "0.2.1"
+__VERSION__ = "1.0.0"
