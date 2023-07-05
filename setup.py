4ffee51bf5d74783dafc733ee7704049ede45f3f
Mark Morley <mmorley0395@gmail.com>
2023-07-05 16:48:57 -0400

    updated setup for proper tag

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='tmc_summarizer',
     packages=find_packages(),
-    version='1.0.3',
-    description='Process raw TMC data into more functional formats',
+    version='1.0.4',
+    description='Process raw TMC data into more functional formats.',
     author='Aaron Fraint, AICP; Mark Morley, AICP', 
     entry_points="""
         [console_scripts]
